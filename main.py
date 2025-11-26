a9684a81823d89ca42bec6eff180506f4323d558
Kauê <kaweddd047@gmail.com>
2025-11-26 14:23:59 -0300

    ajustando tabuada

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from time import sleep
 
 tabuada = 1
+
 while True:
     print('=*' * 25)
     tabuada = int(input('Digite um número para saber a tabuada: '))
     print('=*' * 25)
+
+    if tabuada <= 0:
+        break
 
     print("\033[34mCarregando tabuada", end='')
     print(".", end="")
@@ -14,13 +18,13 @@ while True:
     print(".\033[m")
 
     sleep(1.5)
+
     print('')
 
-    if tabuada <= 0:
-        break
     t = 1
     while t < 11:
         print(f'{tabuada} X {t} = {t*tabuada} ')
         t = t + 1
+        
 print('Fim')
 print('=*' * 25)
